0787000d0c85f849985a24a628fd5dc1f1c9666d
molivpopov <mitkop@mail.bg>
2020-04-11 07:38:22 +0300

    change header editing todo

--- resources/views/toDo/toDo.blade.php
+++ resources/views/toDo/toDo.blade.php
@@ -9,7 +9,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Edit</div>
+                    <div class="card-header">{{$todo ? 'Edit todo #'.$todo->id : 'Add new todo'}}</div>
                     <div class="card-body">
                         <form method="post" action="{{route('apply', ['id' => $todo ? $todo->id : null])}}">
                             @csrf
